937577eceb124b842d35547bb3f721e2543b0d3a
simolorevg <simoneviaggi@libero.it>
2023-06-07 15:49:25 +0200

    trying to fix the migration problem

---
+++ database/seeders/TypesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $newType = new Type();
+            $newType->type = $faker->word();
+            $newType->slug = $faker->text();
+        }
+    }
+}
