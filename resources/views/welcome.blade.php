549161e98e5e4cad3d653a840a08ad0e2a86bf98
simolorevg <simoneviaggi@libero.it>
2023-06-09 13:22:39 +0200

    Made some tyiling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,38 @@
 @section('content')
     <div class="container d-flex flex-column justify-content-center align-items-center">
         @if (empty(Auth::user()->name))
-            <h1>Login or register to make some projects</h1>
+            <h1>Esegui il login o registrati</h1>
         @else
-            <h1>Welcome to my portfolio {{ Auth::user()->name }}</h1>
+            <h1>Benvenuto nel tuo portfolio {{ Auth::user()->name }}</h1>
             <p>Clicca sul menù in alto a destra.</p>
         @endif
+    </div>
+    <div class="slider">
+        <div class="slide-track">
+            <div class="slide">
+                <img src="{{ Vite::asset('resources\imgs\arte.png') }}" height="200" width="200" alt="" />
+            </div>
+            <div class="slide">
+                <img src="{{ Vite::asset('resources\imgs\booleanmarket.png') }}" height="200" width="200"
+                    alt="" />
+            </div>
+            <div class="slide">
+                <img src="{{ Vite::asset('resources\imgs\boolpizza.png') }}" height="200" width="200" alt="" />
+            </div>
+            <div class="slide">
+                <img src="{{ Vite::asset('resources\imgs\booltrain.png') }}" height="200" width="200" alt="" />
+            </div>
+            <div class="slide">
+                <img src="{{ Vite::asset('resources\imgs\boolvie.png') }}" height="200" width="200" alt="" />
+            </div>
+            <div class="slide">
+                <img src="{{ Vite::asset('resources\imgs\array_calcio.png') }}" height="200" width="200"
+                    alt="" />
+            </div>
+            <div class="slide">
+                <img src="{{ Vite::asset('resources\imgs\booltrainlogo2.png') }}" height="200" width="200"
+                    alt="" />
+            </div>
+        </div>
     </div>
 @endsection
