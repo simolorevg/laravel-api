a65400df648e61c7329d488236f2916df846249c
simolorevg <simoneviaggi@libero.it>
2023-06-09 15:34:32 +0200

    Made the edit technologies page

--- resources/views/admin/technologies/edit.blade.php
+++ resources/views/admin/technologies/edit.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.app')
 @section('content')
-    <h2>pagina di edit delle tecnologie</h2>
+    <div class="container">
+        <div class="my-container my-3">
+            <form action="{{ route('admin.technologies.edit', $technology) }}" method="POST" class="d-flex flex-column">
+                @csrf
+                @method('PUT')
+                <label for="typology">Tipologia</label>
+                <input type="text" name="typology" id="typology" value={{ $technology->typology }}>
+                <button type="submit" class="btn btn-primary">INVIA</button>
+            </form>
+        </div>
+    </div>
 @endsection
