60d69ea915e6956930c08e4673d49e8c9394092f
simolorevg <simoneviaggi@libero.it>
2023-06-13 15:09:53 +0200

    Made the public index function on api/ProjectContoller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::all();
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
 }
